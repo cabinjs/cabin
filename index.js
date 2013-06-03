f4a00642ca8203d3c86fff17431b0dd099ac2719
Colin Wren <colinwrenca@gmail.com>
2013-06-03 15:52:45 -0700

    Refactored `cabin new` logic out of utils.js and index.js into new.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var program = require('commander');
-var async = require('async');
-var utils = require('./lib/utils.js');
 
 module.exports = function (argv) {
   program
@@ -13,33 +11,9 @@ module.exports = function (argv) {
     .description('Scaffold out a static site generator')
     .option('-ni, --noInstall', 'don\'t install npm packages')
     .action(function () {
-      var options = {
+      require('./lib/new.js')({
         siteName: program.args[0],
         theme: program.args[1] || 'colinwren/testTheme'
-      };
-
-      utils.getThemeOptions(options.theme, function(err, themeData) {
-        if (err) throw err;
-
-        options.gruntPages = themeData.gruntPages;
-
-        async.series([
-          function (callback) {
-            utils.safePrompt('Which CSS preprocessor will you use?', themeData.style, function (choice) {
-              options.preprocessor = choice;
-              callback();
-            });
-          },
-          function (callback) {
-            utils.safePrompt('Which template language will you use?', themeData.template, function (choice) {
-              options.templateLang = choice;
-              callback();
-            });
-          }
-        ], function () {
-
-          require('./lib/new.js')(options);
-        });
       });
     });
 
