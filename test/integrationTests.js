f7e4dfdf3d08f557232bf287bd0d5aa2e37c7cf3
ChrisWren <cthewren@gmail.com>
2013-08-12 16:46:52 -0700

    Updated integration tests with more tests

--- test/integrationTests.js
+++ test/integrationTests.js
@@ -1,14 +1,14 @@
 'use strict';
 var fs = require('fs');
-var assert = require('assert');
 
 var _ = require('lodash');
+require('should');
 var wrench = require('wrench');
 
 var cabinNew = require('../lib/new.js');
 var siteName = 'testSite';
 
-describe('cabin new <siteName>', function () {
+describe('the cabin new command', function () {
 
   afterEach(function () {
     process.chdir('../');
@@ -17,64 +17,111 @@ describe('cabin new <siteName>', function () {
     }
   });
 
-  describe('when using a remote theme', function () {
+  describe('when using the default theme from its GitHub repo', function () {
 
-    describe('with jade templates', function () {
-
-      it('should create a new site in specified site folder', function (done) {
-
-        testOptions({ templateLang: 'jade' }, function (result) {
-          assert(result.length === 0, result.toString());
-          done();
-        });
+    it('should create a new site generator in the site folder', function (done) {
+      testOptions({}, function (result) {
+        result.length.should.eql(0, result.toString());
+        done();
       });
     });
   });
 
-  describe('when using a local theme', function () {
+  describe('when using a theme from the local filesystem', function () {
 
-    describe('with ejs templates', function () {
+    it('should create a new site generator in the site folder', function (done) {
+      testOptions({
+        theme: 'test/fixtures/candyTheme',
+        local: true
+      }, function (result) {
+        result.length.should.eql(0, result.toString());
+        done();
+      });
+    });
+  });
 
-      it('should create new site in new folder', function (done) {
+  describe('when using any theme', function () {
 
+    describe('when selecting the Jade or EJS template language', function () {
+
+      it('should only copy that template language\'s theme files to the site folder', function (done) {
         testOptions({
-          templateLang: 'ejs',
+          theme: 'test/fixtures/candyTheme',
+          templateLang: 'jade',
+          local: true
+        }, function () {
+          process.exit(1);
+          fs.readFileSync('src/layouts/base.jade').should.be.ok;
+          fs.readFileSync('src/layouts/_header.ejs').should.not.be.ok;
+          done();
+        });
+      });
+
+    });
+
+    describe('when selecting the Sass or LESS style preprocessor', function () {
+
+      it('should only copy that preprocessor language\'s theme files to the site folder', function (done) {
+        testOptions({
+          theme: 'test/fixtures/candyTheme',
+          preprocessor: 'compass',
+          local: true
+        }, function () {
+          fs.readFileSync('src/styles/main.scss').should.be.ok;
+          fs.readFileSync('src/styles/main.less').should.not.be.ok;
+          done();
+        });
+      });
+
+      it('should have that preprocessor\'s grunt task listed as a devDependency', function (done) {
+        testOptions({
+          theme: 'test/fixtures/candyTheme',
+          local: true,
+          preprocessor: 'Sass'
+        }, function () {
+          JSON.parse(fs.readFileSync('./package.json', 'utf8')).devDependencies['grunt-contrib-compass'].should.be.ok;
+          done();
+        });
+      });
+
+      it('should configure that preprocessor\'s grunt task in the Gruntfile', function (done) {
+        testOptions({
+          theme: 'test/fixtures/candyTheme',
+          local: true,
+          preprocessor: 'compass'
+        }, function () {
+          fs.readFileSync('./Gruntfile.js', 'utf8')
+            .should.include('compass');
+          done();
+        });
+      });
+    });
+
+    describe('when run with no grunt pages version specified', function () {
+
+      it('should set the grunt-pages version as `*`', function (done) {
+        testOptions({
+          theme: 'test/fixtures/themeWithNoGruntPagesVersion',
+          local: true
+        }, function () {
+          var actualVersion = JSON.parse(fs.readFileSync('./package.json', 'utf8')).devDependencies['grunt-pages'];
+          actualVersion.should.eql('*', 'grunt-pages version is not `*`');
+          done();
+        });
+      });
+    });
+
+    describe('when run with grunt-pages version specified in the theme', function () {
+
+      it('should set the grunt-pages version to the version in the cabin.json', function (done) {
+        testOptions({
           theme: 'test/fixtures/candyTheme',
           local: true
-        }, function (result) {
-          assert(result.length === 0, result.toString());
+        }, function () {
+          JSON.parse(fs.readFileSync('./package.json', 'utf8')).devDependencies['grunt-pages'].should.eql(
+          JSON.parse(fs.readFileSync('../test/fixtures/candyTheme/cabin.json', 'utf8')).gruntPagesVersion);
           done();
         });
-      });
-    });
-  });
-
-  describe('when run with no grunt pages version specified', function () {
-
-    it('should set the grunt pages version as `*`', function (done) {
-      testOptions({
-        theme: 'test/fixtures/candyTheme',
-        local: true
-      }, function () {
-        var actualVersion = JSON.parse(fs.readFileSync('./package.json', 'utf8')).devDependencies['grunt-pages'];
-        assert(actualVersion === '*', 'grunt-pages version is not `*`');
-        done();
-      });
-    });
-  });
-
-  describe('when run with grunt pages version specified', function () {
-    var gruntPagesVersion = '~4.0.0';
-
-    it('should set the grunt pages version to the version specified in the cabin.json', function (done) {
-      testOptions({
-        gruntPagesVersion: gruntPagesVersion,
-        theme: 'test/fixtures/candyTheme',
-        local: true
-      }, function () {
-        var actualVersion = JSON.parse(fs.readFileSync('./package.json', 'utf8')).devDependencies['grunt-pages'];
-        assert(actualVersion === gruntPagesVersion, 'grunt-pages version is: ' + actualVersion);
-        done();
       });
     });
   });
