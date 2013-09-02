8d5d16575d2923f4e88ac2b4e2643250a57dfde6
ChrisWren <cthewren@gmail.com>
2013-09-02 11:16:46 -0700

    Added additional tests for getThemeData

--- test/unitTests.js
+++ test/unitTests.js
@@ -20,18 +20,26 @@ describe('cabin new', function () {
 
   describe('getThemeData', function () {
 
-    describe('if the data in the json files is formatted correctly', function () {
+    describe('if the data in the cabin.json is formatted correctly', function () {
 
-      it('should return the extracted theme data', function () {
-        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json/good-data').should.eql({
-          style: [ 'Sass' ],
-          template: [ 'Jade' ],
+      it('should return the extracted theme data if the package.json lists grunt-pages as a dev dependency', function () {
+        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json/goodData/gruntPagesDevDep').should.eql({
+          style: ['Sass'],
+          template: ['Jade'],
+          gruntPagesVersion: '0.5.0'
+        });
+      });
+
+      it('should return the extracted theme data if the package.json lists grunt-pages as a hard dependency', function () {
+        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json/goodData/gruntPagesHardDep').should.eql({
+          style: ['Sass'],
+          template: ['Jade'],
           gruntPagesVersion: '0.5.0'
         });
       });
     });
 
-    describe('if the cabin.json or package.json cannot be parsed', function () {
+    describe('if there is an issue reading the theme data', function () {
 
       var prompt = {
         promptFunction: function () {}
@@ -40,8 +48,18 @@ describe('cabin new', function () {
       var promptSpy = sinon.stub(prompt, 'promptFunction');
       newCommand.__set__('invalidThemePrompt', prompt.promptFunction);
 
-      it('should prompt the user to file an issue on the theme\'s GitHub repo', function () {
-        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json');
+      it('should prompt the user to file an issue on the theme\'s GitHub repo when the cabin.json doesn\'t specify a template language', function () {
+        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json/badData/missingSupportedTemplateLang');
+        promptSpy.lastCall.args.should.eql(['testTheme']);
+      });
+
+      it('should prompt the user to file an issue on the theme\'s GitHub repo when the cabin.json doesn\'t specify a preprocessor', function () {
+        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json/badData/missingSupportedPreprocessor');
+        promptSpy.lastCall.args.should.eql(['testTheme']);
+      });
+
+      it('should prompt the user to file an issue on the theme\'s GitHub repo when the package.json doesn\'t have grunt-pages as a dependency', function () {
+        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json/badData/missingGruntPagesDependency');
         promptSpy.lastCall.args.should.eql(['testTheme']);
       });
     });
