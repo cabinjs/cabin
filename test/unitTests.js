783a9bd7137a53a616fff758a3c6145772868d73
ChrisWren <cthewren@gmail.com>
2013-09-02 10:09:16 -0700

    Added unit tests for getThemeData

--- test/unitTests.js
+++ test/unitTests.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var assert = require('assert');
-
 var rewire = require('rewire');
+require('should');
+var sinon = require('sinon');
 
 var newCommand = rewire('../lib/new.js');
 
@@ -11,10 +11,39 @@ describe('cabin new', function () {
   describe('getExcludedExtensions', function () {
 
     it('should return the excluded file extensions based on the user\'s selected options', function () {
-      assert.deepEqual(newCommand.__get__('getExcludedExtensions')({
+      newCommand.__get__('getExcludedExtensions')({
         preprocessorChoice: 'sass',
         templateLangChoice: 'jade'
-      }), ['.less', '.ejs']);
+      }).should.eql(['.less', '.ejs']);
+    });
+  });
+
+  describe('getThemeData', function () {
+
+    describe('if the data in the json files is formatted correctly', function () {
+
+      it('should return the extracted theme data', function () {
+        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json/good-data').should.eql({
+          style: [ 'Sass' ],
+          template: [ 'Jade' ],
+          gruntPagesVersion: '0.5.0'
+        });
+      });
+    });
+
+    describe('if the cabin.json or package.json cannot be parsed', function () {
+
+      var prompt = {
+        promptFunction: function () {}
+      };
+
+      var promptSpy = sinon.stub(prompt, 'promptFunction');
+      newCommand.__set__('invalidThemePrompt', prompt.promptFunction);
+
+      it('should prompt the user to file an issue on the theme\'s GitHub repo', function () {
+        newCommand.__get__('getThemeData')('testTheme', 'test/fixtures/json');
+        promptSpy.lastCall.args.should.eql(['testTheme']);
+      });
     });
   });
 });
