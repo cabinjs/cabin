deac91fd91b97e87e4ac10ee9589ea343c726c6b
niftylettuce <niftylettuce@gmail.com>
2017-11-11 20:40:11 -0500

    Added public website for docs

---
+++ config.js
@@ -0,0 +1,16 @@
+docute.init({
+  debug: true,
+  title: 'Cabin',
+  repo: 'cabinjs/cabin',
+  'edit-link': 'https://github.com/cabinjs/cabin/tree/master/',
+  twitter: 'niftylettuce',
+  nav: {
+    default: [
+      {
+        title:
+          'Logging/analytics service and middleware for Node.js, Lad, Koa, and Express',
+        path: '/'
+      }
+    ]
+  }
+});
